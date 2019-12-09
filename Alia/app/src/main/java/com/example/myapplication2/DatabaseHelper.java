74b382cd88e75807e9439201aec0ae0a14af20f1
arlinpotato <44635253+arlinpotato@users.noreply.github.com>
2019-12-09 12:45:18 +0800

    Added Firebase, Setup Cloud Firestore

--- Alia/app/src/main/java/com/example/myapplication2/DatabaseHelper.java
+++ Alia/app/src/main/java/com/example/myapplication2/DatabaseHelper.java
@@ -7,13 +7,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String DATABASE_NAME="RecipeBook.db";
+    public static final String TABLE_NAME ="recipe_table";
+    public static final String COL_1 = "title";
+    public static final String COL_2 = "ingredient";
+    public static final String COL_3 = "instruction";
+    public static final String COL_4 = "cookingtime";
+    public static final String COL_5 = "rating";
+    public static final String COL_6 = "nutrition";
+    public static final String COL_7 = "calories";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table" + TABLE_NAME + "(title, )");
     }
 
     @Override
---
+++ Alia/app/src/main/java/com/example/myapplication2/DocSnippets.java
@@ -0,0 +1,8 @@
+package com.example.myapplication2;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class DocSnippets {
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+}
